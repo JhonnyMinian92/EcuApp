6ecba5983114e9494b3997ef4615293fbf696f8b
Jhonny Miñan <jhonnyminian@gmail.com>
2023-01-19 12:39:18 -0500

    creacion de proyecto
    creacion de directorios y archivos inciales

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>EcuApp</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <article></article>
+            </nav>
+        </header>
+        <section>
+            <article></article>
+        </section>
+        <footer>
+            <article></article>
+        </footer>
+    </body>
+</html>
