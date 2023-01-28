d3a04b4e4e540e7c8c12a919db6458e281260e74
Jhonny Miñan <jhonnyminian@gmail.com>
2023-01-28 12:37:54 -0500

    responsive
    html responsive

--- vista/contenedor.php
+++ vista/contenedor.php
@@ -10,7 +10,7 @@ $urlcss = "../../pack/css/";
 
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">      
         <title>EcuApp</title>
     </head>
     
