1564993145204e95754c0ab35eae74cc198ff256
Jhonny Miñan <53890778+JhonnyMinian92@users.noreply.github.com>
2023-03-10 11:15:47 -0500

    login js
    codigo para funcionar con ajax y servicios rest para javascript sesion

--- vista/buscador.php
+++ vista/buscador.php
@@ -1,8 +1,14 @@
 <html>
     <head>
+        <style>
+            #cuerpo {
+                margin-top: 20px;
+            }
+        </style>
     </head>
     <body>
-        <button id="elboton" onclick="Iniciar();">Login</button>
+        <button id="btnlogin" onclick="Iniciar();">Login</button>
+        <button id="deslogin" onclick="Cerrar();">Cerrar</button>
 
             <section id="cuerpo">
                     <input placeholder="Palabra a Buscar"/>
@@ -10,22 +16,60 @@
             </section>
 
             <script>
-                if (sessionStorage.getItem('Idlogueo') === 'true') {
+                if (sessionStorage.getItem('Statuslogueo') === 'true') {
                     alert("Sesion se mantiene inciada");
                     document.getElementById("cuerpo").style.display = "block";
-                    document.getElementById("elboton").style.display = "none";
+                    document.getElementById("btnlogin").style.display = "none";
+                    document.getElementById("deslogin").style.display = "block";
                 }
                 else {
                     alert("Sesion no existe");
                     document.getElementById("cuerpo").style.display = "none";
+                    document.getElementById("deslogin").style.display = "none";
+                }
+
+                function logueo(){
+                    //ajax para servicio de logueo
+                    return true;
+                }
+
+                function Token(){
+                    //ajax para servicio de 2do factor de autenticacion
+                    return "937172385";
                 }
                 
                 function Iniciar(){
                     alert("Sesion inciada");
-                    sessionStorage.setItem('Idlogueo', true);
-                    document.getElementById("cuerpo").style.display = "block";
-                    document.getElementById("elboton").style.display = "none";
+                    var status = logueo();
+                    if(status){
+                        sessionStorage.setItem('Statuslogueo', status);
+                        var IdLogin = Token();
+                        if(IdLogin != ""){
+                            //obtener el id del usuario luego de pasar por el proceso de token
+                            sessionStorage.setItem('IdLogueo', IdLogin);
+                            //mostrar formulario
+                            document.getElementById("cuerpo").style.display = "block";
+                            document.getElementById("btnlogin").style.display = "none";
+                            document.getElementById("deslogin").style.display = "block";
+                            //codigo para que mostrar toda la info del usuario IdUsuario
+                        } else {
+                            alert("Codigo Incorrecto");
+                        }
+                    } else { alert("Usuario o Contraseña Incorrecta"); }
                 }
+
+                function Cerrar(){
+                    if(sessionStorage.getItem('IdLogueo') == '937172385'){
+                        alert("Sesion cerrada");
+                        sessionStorage.removeItem("Statuslogueo");
+                        document.getElementById("cuerpo").style.display = "none";
+                        document.getElementById("btnlogin").style.display = "block";
+                        document.getElementById("deslogin").style.display = "none";
+                    } else {
+                        alert("Sesion Incorrecta");
+                    }
+                }
+
             </script>
 
     </body>
