8fc7a43b351e5e8854e8effb7b3342702c14820e
Jhonny Miñan <53890778+JhonnyMinian92@users.noreply.github.com>
2023-03-16 23:49:20 -0500

    componentes-login
    crear la estructura de componentes para el login, registro o recuperar clave

--- pack/js/componentes/viewregistro.js
+++ pack/js/componentes/viewregistro.js
@@ -1,4 +1,59 @@
 function btnlogin() {
     //front para loguearse/registrarse
    Contenido("cuerpogeneral","");
+   //crear conteniido
+   var cuerpo = Componente("cuerpogeneral");
+   var div = CrearObjeto("div");
+   SaveObjeto(cuerpo,div);
+   //crear cuadro de login
+   var article = CrearObjeto("article");
+   AddAtributo(article,"Class","cjlogueo");
+   SaveObjeto(div,article);
+   //crear cajas con componentes
+   //caja para icono
+   var div = CrearObjeto("div");
+   SaveObjeto(article,div);
+   var logo = CrearObjeto("div");
+   AddAtributo(logo,"Class","img-logueo");
+   SaveObjeto(div,logo);  
+   //caja para correo
+   var div = CrearObjeto("div");
+   SaveObjeto(article,div);
+   var input = CrearObjeto("input");
+   AddAtributo(input,"Class","text-correo");
+   SaveObjeto(div,input);
+   //caja para clave
+   var div = CrearObjeto("div");
+   SaveObjeto(article,div);
+   var input = CrearObjeto("input");
+   AddAtributo(input,"Class","text-clave");
+   SaveObjeto(div,input);
+   //caja para olvido
+   var div = CrearObjeto("div");
+   AddAtributo(div,"Class","cj-opciones");
+   SaveObjeto(article,div);
+   var hiper = CrearObjeto("div");
+   AddAtributo(hiper,"Class","lb-olvido");
+   ValorTexto(hiper,"¿ Olvidó su contraseña ?");
+   SaveObjeto(div,hiper);
+   var registro = CrearObjeto("div");
+   AddAtributo(registro,"Class","lb-singup");
+   ValorTexto(registro,"Crear Cuenta");
+   SaveObjeto(div,registro);
+   //caja boton de logueo
+   var div = CrearObjeto("div");
+   SaveObjeto(article,div);
+   var boton = CrearObjeto("div");
+   AddAtributo(boton,"Class","btn-loguear");
+   AddAtributo(boton,"onclick","IngresarLogin();");
+   ValorTexto(boton,"Ingresar");
+   SaveObjeto(div,boton);
+}
+
+function btnregister(){
+
+}
+
+function btnolvido(){
+    
 }
